a0401d1cf9fd8a8e63591e6c47241ae9dfb3f2a4
Chris Maddern <chris@venmo.com>
2013-12-23 19:51:37 +0000

    Formatting

--- VENSnowOverlayView.m
+++ VENSnowOverlayView.m
@@ -18,13 +18,13 @@
         self.backgroundColor    = [UIColor clearColor];
         
         // Set default values
-		self.flakesCount        = kDefaultFlakesCount;
-		self.flakeWidth         = kDefaultFlakeWidth;
-		self.flakeHeight        = kDefaultFlakeHeight;
-		self.flakeFileName      = kDefaultFlakeFileName;
-		self.flakeMinimumSize   = kDefaultMinimumSize;
-		self.animationDurationMin = kDefaultAnimationDurationMin;
-		self.animationDurationMax = kDefaultAnimationDurationMax;
+        self.flakesCount        = kDefaultFlakesCount;
+        self.flakeWidth         = kDefaultFlakeWidth;
+        self.flakeHeight        = kDefaultFlakeHeight;
+        self.flakeFileName      = kDefaultFlakeFileName;
+        self.flakeMinimumSize   = kDefaultMinimumSize;
+        self.animationDurationMin = kDefaultAnimationDurationMin;
+        self.animationDurationMax = kDefaultAnimationDurationMax;
     }
     return self;
 }
@@ -37,32 +37,32 @@
     [[NSNotificationCenter defaultCenter] addObserver:self selector:@selector(endSnowAnimationFromNotification:) name:UIApplicationDidEnterBackgroundNotification object:nil];
     
     // Prepare Rotation Animation
-	CABasicAnimation *rotationAnimation = [CABasicAnimation animationWithKeyPath:@"transform.rotation.y"];
-	rotationAnimation.repeatCount       = 1e100;
-	rotationAnimation.autoreverses      = NO;
-	rotationAnimation.toValue = [NSNumber numberWithFloat:6.28318531];	// 360 degrees in radians
+    CABasicAnimation *rotationAnimation = [CABasicAnimation animationWithKeyPath:@"transform.rotation.y"];
+    rotationAnimation.repeatCount       = 1e100;
+    rotationAnimation.autoreverses      = NO;
+    rotationAnimation.toValue = [NSNumber numberWithFloat:6.28318531];	// 360 degrees in radians
 	
     // Prepare Vertical Motion Animation
-	CABasicAnimation *fallAnimation = [CABasicAnimation animationWithKeyPath:@"transform.translation.y"];
-	fallAnimation.repeatCount       = 1e100;
-	fallAnimation.autoreverses      = NO;
+    CABasicAnimation *fallAnimation = [CABasicAnimation animationWithKeyPath:@"transform.translation.y"];
+    fallAnimation.repeatCount       = 1e100;
+    fallAnimation.autoreverses      = NO;
 	
 	for (UIImageView *flake in self.flakesArray) {
-		CGPoint flakeStartPoint     = flake.center;
-		float flakeStartY           = flakeStartPoint.y;
-		float flakeEndY             = self.frame.size.height;
-		flakeStartPoint.y           = flakeEndY;
-		flake.center                = flakeStartPoint;
+        CGPoint flakeStartPoint     = flake.center;
+        float flakeStartY           = flakeStartPoint.y;
+        float flakeEndY             = self.frame.size.height;
+        flakeStartPoint.y           = flakeEndY;
+        flake.center                = flakeStartPoint;
         
         // Randomize the time each flake takes to animate to give texture
-		float timeInterval = ((Float32)(self.animationDurationMax - self.animationDurationMin) * (Float32)random() / (Float32)RAND_MAX);
-		fallAnimation.duration = timeInterval + self.animationDurationMin;
-		fallAnimation.fromValue = [NSNumber numberWithFloat:-flakeStartY];
-		[flake.layer addAnimation:fallAnimation forKey:@"transform.translation.y"];
+        float timeInterval = ((Float32)(self.animationDurationMax - self.animationDurationMin) * (Float32)random() / (Float32)RAND_MAX);
+        fallAnimation.duration = timeInterval + self.animationDurationMin;
+        fallAnimation.fromValue = [NSNumber numberWithFloat:-flakeStartY];
+        [flake.layer addAnimation:fallAnimation forKey:@"transform.translation.y"];
 		
-		rotationAnimation.duration = timeInterval * 2; // Makes sure that we don't get super-fast spinning flakes
-		[flake.layer addAnimation:rotationAnimation forKey:@"transform.rotation.y"];
-	}
+        rotationAnimation.duration = timeInterval * 2; // Makes sure that we don't get super-fast spinning flakes
+        [flake.layer addAnimation:rotationAnimation forKey:@"transform.rotation.y"];
+    }
 }
 
 
@@ -88,7 +88,7 @@
         UIImage *flakeImg = [UIImage imageNamed:self.flakeFileName];
         for (int i=0; i < self.flakesCount; i++) {
             
-			// Randomize Flake size
+            // Randomize Flake size
             float flakeScale = ((Float32)1.0*(Float32)random()/(Float32)RAND_MAX);
             
             // Make sure that we don't break the current size rules
@@ -96,13 +96,13 @@
             float flakeWidth    = self.flakeWidth * flakeScale;
             float flakeHeight   = self.flakeHeight * flakeScale;
             
-			// Allow flakes to be partially offscreen
+            // Allow flakes to be partially offscreen
             float flakeXPosition = ((Float32)self.frame.size.width*(Float32)random()/(Float32)RAND_MAX);
             flakeXPosition -= flakeWidth;
             
-			// enlarge content height by 1/2 view height, screen is always well populated
+            // enlarge content height by 1/2 view height, screen is always well populated
             float flakeYPosition = ((Float32)self.frame.size.height*1.5*(Float32)random()/(Float32)RAND_MAX);
-			// flakes start y position is above upper view bound, add view height
+            // flakes start y position is above upper view bound, add view height
             flakeYPosition += self.frame.size.height;
             
             CGRect frame = CGRectMake(flakeXPosition, flakeYPosition, flakeWidth, flakeHeight);
@@ -114,7 +114,7 @@
             [self.flakesArray addObject:imageView];
             [self addSubview:imageView];
         }
-	}
+    }
     return _flakesArray;
 }
 
